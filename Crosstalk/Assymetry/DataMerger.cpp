80e3bc7a9cd7a7c8b8ad83b34e0659ff5a4cf812
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2019-09-11 11:59:18 +0200

    Add files via upload

---
+++ Crosstalk/Assymetry/DataMerger.cpp
@@ -0,0 +1,145 @@
+#include <TH2F.h>
+#include <fstream>
+#include <iostream>
+#include "TStyle.h"
+#include "TCutG.h"
+
+void DataMerger()
+{
+    gStyle->SetOptStat(1111);
+    gStyle->SetOptFit(111);
+    gStyle->SetPalette(1,0);
+    gStyle->SetPadColor(0);
+    gStyle->SetCanvasColor(0);
+    
+    const int NumberOfFiles=2;
+    char filename[NumberOfFiles][200];
+    sprintf(filename[0],"/Users/kolos/Desktop/Studia/CIS/Crosstalk/StoppingProton/Assymetry/Data/27August_1_MCR0_hadrons_0pt8Gev_0pt0T_Beam___CrosstalkAsymmetry.root");
+    sprintf(filename[1],"/Users/kolos/Desktop/Studia/CIS/Crosstalk/StoppingProton/Assymetry/Data/26August_26_MCR0_hadrons_0pt8Gev_0pt0T_Beam___CrosstalkAsymmetry.root"); 
+    
+    TH2F *NewEventsMap_XY = new TH2F("NewEventsMap_XY","NewEventsMap_XY",  24,0,24, 8,0,8);
+    TH2F *NewEventsMap_YZ = new TH2F("NewEventsMap_YZ","NewEventsMap_YZ",  48,0,48, 8,0,8);
+    TH2F *NewEventsMap_XZ = new TH2F("NewEventsMap_XZ","NewEventsMap_XZ",  24,0,24, 48,0,48);
+    
+    TH2F *NewEnergyTrigTimeAll = new TH2F("NewEnergyTrigTimeAll","Energy and Time of Trigger whole run", 100,-100,100,500,0,1500);
+    
+    TH1F *NewHistogramHighestEnergyDeposit = new TH1F("NewHistogramHighestEnergyDeposit", "Histogram of highest value deposit",100,0,2500);
+    TH1F *NewHistogramRealPeakEnergyDeposit = new TH1F("NewHistogramRealPeakEnergyDeposit", "Histogram of found stopping proton deposit",100,0,2500);
+    TH1F *NewStoppingPointLocation = new TH1F("NewStoppingPointLocation", "Location of stopping point",48,0,48);
+
+    TH1F *NewCrosstalkEnergyDepositX = new TH1F("NewCrosstalkEnergyDepositX", "Crosstalk energy in X plain",500,0,500);
+    TH1F *NewCrosstalkEnergyDepositY = new TH1F("NewCrosstalkEnergyDepositY", "Crosstalk energy in Y plain",500,0,500);
+    
+    TH1F *NewCrosstalkHistogramDifferenceX = new TH1F("NewCrosstalkHistogramDifferenceX", "Crosstalk right subtracted by crosstalk on the left in X plain",100.0,-50.0,50.0);
+
+    for(int i=0; i<NumberOfFiles; i++)
+    {
+        TFile *file = new TFile(filename[i],"READ");
+        if ( file->IsOpen() )
+        {
+            printf("File opened successfully\n");
+        }   
+        //file->cd();
+        
+        TH2F* EventsMap_XYClone = (TH2F*) file->Get("EventsMap_XY");
+        NewEventsMap_XY->Add(EventsMap_XYClone);
+        TH2F* EventsMap_YZClone = (TH2F*) file->Get("EventsMap_YZ");
+        NewEventsMap_YZ->Add(EventsMap_YZClone);
+        TH2F* EventsMap_XZClone = (TH2F*) file->Get("EventsMap_XZ");
+        NewEventsMap_XZ->Add(EventsMap_XZClone);
+        
+        TH2F* EnergyTrigTimeAllClone = (TH2F*) file->Get("EnergyTrigTimeAll");
+        NewEnergyTrigTimeAll->Add(EnergyTrigTimeAllClone);
+
+        TH1F* HistogramHighestEnergyDepositClone = (TH1F*) file->Get("HistogramHighestEnergyDeposit");
+        NewHistogramHighestEnergyDeposit->Add(HistogramHighestEnergyDepositClone);
+        
+        TH1F* HistogramRealPeakEnergyDepositClone = (TH1F*) file->Get("HistogramRealPeakEnergyDeposit");
+        NewHistogramRealPeakEnergyDeposit->Add(HistogramRealPeakEnergyDepositClone);
+        
+        TH1F* StoppingPointLocationClone = (TH1F*) file->Get("StoppingPointLocation");
+        NewStoppingPointLocation->Add(StoppingPointLocationClone);
+        
+        TH1F* CrosstalkEnergyDepositXClone = (TH1F*) file->Get("CrosstalkEnergyDepositX");
+        NewCrosstalkEnergyDepositX->Add(CrosstalkEnergyDepositXClone);
+        TH1F* CrosstalkEnergyDepositYClone = (TH1F*) file->Get("CrosstalkEnergyDepositY");
+        NewCrosstalkEnergyDepositY->Add(CrosstalkEnergyDepositYClone);
+        
+        TH1F* CrosstalkHistogramDifferenceXClone = (TH1F*) file->Get("CrosstalkHistogramDifferenceX");
+        NewCrosstalkHistogramDifferenceX->Add(CrosstalkHistogramDifferenceXClone);
+        
+        delete EventsMap_XYClone;
+        delete EventsMap_YZClone;
+        delete EventsMap_XZClone;
+        delete EnergyTrigTimeAllClone;
+        delete HistogramHighestEnergyDepositClone;
+        delete HistogramRealPeakEnergyDepositClone;
+        delete StoppingPointLocationClone;
+        delete CrosstalkEnergyDepositXClone;
+        delete CrosstalkEnergyDepositYClone;
+        delete CrosstalkHistogramDifferenceXClone;
+        
+        file->Close();
+        delete file;
+    }
+    
+    TH2F *EventsMap_XY = (TH2F*)(NewEventsMap_XY->Clone("EventsMap_XY"));
+    TH2F *EventsMap_YZ = (TH2F*)(NewEventsMap_YZ->Clone("EventsMap_YZ"));
+    TH2F *EventsMap_XZ = (TH2F*)(NewEventsMap_XZ->Clone("EventsMap_XZ"));
+
+    TH2F *EnergyTrigTimeAll = (TH2F*)(NewEnergyTrigTimeAll->Clone("EnergyTrigTimeAll"));
+    EnergyTrigTimeAll->GetYaxis()->SetTitle("Energy [p.e.]");
+    EnergyTrigTimeAll->GetXaxis()->SetTitle("Time from Trigger [2.5 ns]");
+
+    TH1F *HistogramHighestEnergyDeposit = (TH1F*)(NewHistogramHighestEnergyDeposit->Clone("HistogramHighestEnergyDeposit"));
+    HistogramHighestEnergyDeposit->GetYaxis()->SetTitle("Number of events");
+    HistogramHighestEnergyDeposit->GetXaxis()->SetTitle("Energy [p.e.]");
+    
+    TH1F *HistogramRealPeakEnergyDeposit = (TH1F*)(NewHistogramRealPeakEnergyDeposit->Clone("HistogramRealPeakEnergyDeposit"));
+    HistogramRealPeakEnergyDeposit->GetYaxis()->SetTitle("Number of events");
+    HistogramRealPeakEnergyDeposit->GetXaxis()->SetTitle("Energy [p.e.]");
+    
+    TH1F *StoppingPointLocation = (TH1F*)(NewStoppingPointLocation->Clone("StoppingPointLocation"));
+    StoppingPointLocation->GetYaxis()->SetTitle("Number of events");
+    StoppingPointLocation->GetXaxis()->SetTitle("Energy [p.e.]");
+    
+    TH1F *CrosstalkEnergyDepositX = (TH1F*)(NewCrosstalkEnergyDepositX->Clone("CrosstalkEnergyDepositX"));
+    CrosstalkEnergyDepositX->GetYaxis()->SetTitle("Number of events");
+    CrosstalkEnergyDepositX->GetXaxis()->SetTitle("Energy [p.e.]");
+    TH1F *CrosstalkEnergyDepositY = (TH1F*)(NewCrosstalkEnergyDepositY->Clone("CrosstalkEnergyDepositY"));
+    CrosstalkEnergyDepositY->GetYaxis()->SetTitle("Number of events");
+    CrosstalkEnergyDepositY->GetXaxis()->SetTitle("Energy [p.e.]");
+    
+    TH1F *CrosstalkHistogramDifferenceX = (TH1F*)(NewCrosstalkHistogramDifferenceX->Clone("CrosstalkHistogramDifferenceX"));
+    CrosstalkHistogramDifferenceX->GetYaxis()->SetTitle("Number of events");
+    CrosstalkHistogramDifferenceX->GetXaxis()->SetTitle("Energy [p.e.]");
+    
+    TFile *fileout = new TFile("/Users/kolos/Desktop/Studia/CIS/Crosstalk/StoppingProton/Assymetry/Data/MergedCrosstalk.root","RECREATE");
+    if ( fileout->IsOpen() )
+    {
+        printf("File opened successfully\n");
+    }  
+        
+    fileout->cd();
+    
+    EventsMap_XY->Write();
+    EventsMap_YZ->Write();
+    EventsMap_XZ->Write();
+    
+    EnergyTrigTimeAll->Write();
+    
+    HistogramHighestEnergyDeposit->Write();
+    HistogramRealPeakEnergyDeposit->Write();
+    
+    StoppingPointLocation->Write();
+    
+    CrosstalkEnergyDepositX->Write();
+    CrosstalkEnergyDepositY->Write();
+    
+    CrosstalkHistogramDifferenceX->Write();
+    cout<<"udalo sie"<<endl;
+    
+    fileout->Close();
+    delete fileout;
+        
+}
